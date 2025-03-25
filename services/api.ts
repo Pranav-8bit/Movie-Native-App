dc7f91d54342dde7e18ee111dfcf5631d6e5c2b2
Pranav-8bit <tpranav15@gmail.com>
2025-03-25 22:03:46 +0530

    comment removal

--- services/api.ts
+++ services/api.ts
@@ -22,19 +22,3 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   const data = await response.json();
   return data.results;
 };
-
-// const url =
-//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-// const options = {
-//   method: "GET",
-//   headers: {
-//     accept: "application/json",
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYWJhMDhkYmEzNzM1ZjA5Y2E0YWZmNzRjNTUyNmU4YyIsIm5iZiI6MTY2Mzk1NjIzOC4wNzksInN1YiI6IjYzMmRmNTBlZTEwZjQ2MDA3YTIwYjkzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K7Q0VU4aRiR8k-n72scokuRNObnRWcNL5svUKFqP7MA",
-//   },
-// };
-
-// fetch(url, options)
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-//   .catch((err) => console.error(err));
