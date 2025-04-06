9a3b46c4acdd364d09be6c76de98a1663115d4ad
Pranav-8bit <tpranav15@gmail.com>
2025-04-06 16:57:55 +0530

    Trending movie slider and detail Movie Page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,85 +1,104 @@
-import {View, Text, ImageBackground, Image} from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
 import React from "react";
-import {Tabs} from "expo-router";
-import {images} from "@/constants/images";
-import {icons} from "@/constants/icons";
+import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
-const TabIcon = ({Icon, tabName, focussed}: any) => {
-    if (focussed) {
-        return (
-            <ImageBackground
-                source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px]
-                min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
-                <Image source={Icon} tintColor="#151312" className="size-5"/>
-                <Text className="text-secondary text-base font-semibold ml-2">{tabName}</Text>
-            </ImageBackground>
-        )
-    }
-
+const TabIcon = ({ Icon, tabName, focussed }: any) => {
+  if (focussed) {
     return (
-        <View className="size-full justify-center items-center mt-4 rounded-full">
-            <Image source={Icon} tintColor="#A8B5DB" className="size-5"/>
-        </View>
-    )
-}
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px]
+                min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
+        <Image source={Icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {tabName}
+        </Text>
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={Icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+};
 
 const _layout = () => {
-    return (
-        <Tabs screenOptions={{
-            tabBarShowLabel: false,
-            tabBarItemStyle: {width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center'},
-            tabBarStyle: {
-                backgroundColor: "#0f0D23",
-                borderRadius: 50,
-                marginHorizontal: 20,
-                marginBottom: 36,
-                height: 52,
-                position: "absolute",
-                overflow: "hidden",
-                borderWidth: 1,
-                borderColor: '#0f0d23'
-            }
-        }}>
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: "home",
-                    headerShown: false,
-                    tabBarIcon: ({focused}) => (
-                        <TabIcon Icon={icons.home} tabName={"Home"}
-                                 focussed={focused}/>)
-                }}
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23",
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon Icon={icons.home} tabName={"Home"} focussed={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="SearchScreen"
+        options={{
+          title: "SearchScreen",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              Icon={icons.search}
+              tabName={"Search"}
+              focussed={focused}
             />
-            <Tabs.Screen
-                name="search"
-                options={{
-                    title: "search", headerShown: false,
-                    tabBarIcon: ({focused}) => (
-                        <TabIcon Icon={icons.search} tabName={"Search"}
-                                 focussed={focused}/>)
-                }}
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon Icon={icons.save} tabName={"Saved"} focussed={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              Icon={icons.person}
+              tabName={"Profile"}
+              focussed={focused}
             />
-            <Tabs.Screen
-                name="saved"
-                options={{
-                    title: "saved", headerShown: false,
-                    tabBarIcon: ({focused}) => (
-                        <TabIcon Icon={icons.save} tabName={"Saved"}
-                                 focussed={focused}/>)
-                }}
-            />
-            <Tabs.Screen
-                name="profile"
-                options={{
-                    title: "profile", headerShown: false,
-                    tabBarIcon: ({focused}) => (
-                        <TabIcon Icon={icons.person} tabName={"Profile"}
-                                 focussed={focused}/>)
-                }}
-            />
-        </Tabs>
-    );
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default _layout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,15 +14,27 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { trendingMovies } from "@/services/appwrite";
+import TrendCard from "@/components/TrendCard";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendMovies,
+    loading: trendLoading,
+    error: trendError,
+  } = useFetch(() => trendingMovies());
 
   const {
     data: movies,
     loading: movieLoading,
     error: movieError,
   } = useFetch(() => fetchMovies({ query: "" }));
+
+  const navigateToSearch = () => {
+    router.push("/SearchScreen");
+  };
 
   return (
     <View className="flex-1 bg-primary">
@@ -32,25 +44,47 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {movieLoading ? (
+        {movieLoading || trendLoading ? (
           <ActivityIndicator
             size="large"
             color="000ff"
             className="mt-10 self-center"
           />
-        ) : movieError ? (
-          <Text>Error: {movieError.message}</Text>
+        ) : movieError || trendError ? (
+          <Text>Error: {movieError?.message || trendError?.message}</Text>
         ) : (
           //main content
           <View className="flex-1 mt-5">
             <Searchbar
-              onPress={() => router.push("/search")}
+              onPress={navigateToSearch}
               placeholder="Search for a movie"
             />
+
+            {trendMovies && (
+              <>
+                <View className="my-10">
+                  <Text className="text-lg text-white font-bold mb-3">
+                    Trending Movies
+                  </Text>
+                </View>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                  data={trendMovies}
+                  className="mb-4 mt-3"
+                  renderItem={({ item, index }) => (
+                    <TrendCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                />
+              </>
+            )}
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest Movie
               </Text>
+
               <FlatList
                 data={movies}
                 renderItem={({ item }) => <MovieCard {...item} />}
